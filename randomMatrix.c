c3a4c27f69df4e858dfb2539885127c4fc5c4596
Tito Yak <titokuol@college.harvard.edu>
2021-03-26 16:28:42 +0300

    - generate NxN matrix

--- randomMatrix.c
+++ randomMatrix.c
@@ -9,3 +9,17 @@ typedef struct matrix {
 	int** matrix_array;
 	int matrix_size;
 } matrix;
+
+
+matrix* generateMatrix(int n) {
+	matrix* n_matrix = malloc(sizeof(matrix));
+	n_matrix->col_1 = 0;
+	n_matrix->row_1 = 0;
+	n_matrix->matrix_size = n;
+	n_matrix->matrix_array = malloc(sizeof(int*) * n);
+	for (int i = 0; i < n; i++) {
+		n_matrix->matrix_array[i] = calloc(n, sizeof(int));
+	}
+	return n_matrix;
+}
+
