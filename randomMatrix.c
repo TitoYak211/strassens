b6e4b034bec40d2e819f3de739560aa1d8d9be8a
Tito Yak <titokuol@college.harvard.edu>
2021-03-26 16:41:36 +0300

    Update randomMatrix.c

--- randomMatrix.c
+++ randomMatrix.c
@@ -10,29 +10,24 @@ typedef struct matrix {
 	int matrix_size;
 } matrix;
 
-
-matrix* generateMatrix(int n) {
+matrix* randomMatrix(int n) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    srand((unsigned) tv.tv_usec);
 	matrix* n_matrix = malloc(sizeof(matrix));
 	n_matrix->col_1 = 0;
 	n_matrix->row_1 = 0;
 	n_matrix->matrix_size = n;
 	n_matrix->matrix_array = malloc(sizeof(int*) * n);
+
 	for (int i = 0; i < n; i++) {
 		n_matrix->matrix_array[i] = calloc(n, sizeof(int));
 	}
-	return n_matrix;
-}
 
-
-matrix* randomMatrix(int n) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    srand((unsigned) tv.tv_usec);
-	matrix* n_matrix = generateMatrix(n);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			int e = rand() % 3 - 1;
-			n_matrix->matrix_array[n_matrix->row_1 + i][n_matrix->col_1 + j] = e;
+			int cell_val = rand() % 3 - 1;
+			n_matrix->matrix_array[n_matrix->row_1 + i][n_matrix->col_1 + j] = cell_val;
 		}
 	}
 	return n_matrix;
